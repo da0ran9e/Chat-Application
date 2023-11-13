83d4357b37562c40db7b661e2175bfbf1f197b15
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-11-13 14:28:41 +0700

    Update start.c

--- gui/start.c
+++ gui/start.c
@@ -10,7 +10,7 @@ int main (int argc, char ** argv){
 
 	int ret;
 
-	app = gtk_application_new ("in.start", G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new ("in.start", G_APPLICATION_DEFAULT_FLAGS);
 
 	g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
 
