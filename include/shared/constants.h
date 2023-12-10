94634434350fe16776ac7ec6a27f853195919877
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-10 20:49:39 +0700

    Update constants.h

---
+++ include/shared/constants.h
@@ -0,0 +1,20 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+enum Object {
+    SERVER,
+    CLIENT
+}
+
+enum ServerStatus {
+    RUNNING,
+    CLOSED,
+};
+
+enum ClientStatus {
+    LOGIN,
+    LOGOUT,
+    REGISTER,
+}
+
+#endif
