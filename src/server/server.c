54f155743f106ab7785b78dacf9e442d844abbc1
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-11 23:26:37 +0700

    Update server.c

--- src/server/server.c
+++ src/server/server.c
@@ -6,16 +6,12 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define BUFFER 1024
-#include "../../include/server.h"
-#include "../../include/shared/constants.h"
-// #include "../../include/server/log.h"
 
-#define MAX_CLIENTS 10
-
-ssize_t receiveMessage (int clientSocket, char *buffer) {
-    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+ssize_t receiveMessage(int clientSocket, char *buffer) {
+    ssize_t bytesRead = recv(clientSocket, buffer, BUFFER - 1, 0);
     if (bytesRead <= 0) {
         perror("Connection lost from: ");
         return -1;
@@ -26,7 +22,7 @@ ssize_t receiveMessage (int clientSocket, char *buffer) {
     return bytesRead;
 }
 
-ssize_t sendMessage (int clientSocket, const void *message, size_t len) {
+ssize_t sendMessage(int clientSocket, const void *message, size_t len) {
     size_t totalSent = 0;
 
     while (totalSent < len) {
@@ -37,7 +33,7 @@ ssize_t sendMessage (int clientSocket, const void *message, size_t len) {
             return -1;
         } else if (sent == 0) {
             fprintf(stderr, "Client %d is not online!\n", clientSocket);
-            return 0; 
+            return 0;
         }
 
         totalSent += sent;
@@ -46,35 +42,35 @@ ssize_t sendMessage (int clientSocket, const void *message, size_t len) {
     return totalSent;
 }
 
-void initializeServer(int serverSocket) {
+void initializeServer(int *serverSocket, int port) {
     struct sockaddr_in serverAddr;
 
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((*serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("Error creating server socket");
         exit(EXIT_FAILURE);
     }
 
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(serverSocket);
+    serverAddr.sin_port = htons(port);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+    if (bind(*serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
         perror("Error binding server socket");
-        close(serverSocket);
+        close(*serverSocket);
         exit(EXIT_FAILURE);
     }
 
-    if (listen(serverSocket, 5) == -1) {
+    if (listen(*serverSocket, 5) == -1) {
         perror("Error listening for connections");
-        close(serverSocket);
+        close(*serverSocket);
         exit(EXIT_FAILURE);
     }
 
-    printf("Server is listening on port %d...\n", serverSocket);
+    printf("Server is listening on port %d...\n", port);
 }
 
-void runServer(int serverSocket){
+void runServer(int serverSocket) {
     int clientSockets[MAX_CLIENTS];
     fd_set readfds, allset;
     struct sockaddr_in clientAddr;
@@ -101,7 +97,7 @@ void runServer(int serverSocket){
         // Check for incoming connection on the server socket
         if (FD_ISSET(serverSocket, &readfds)) {
             // Accept the connection
-            int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addrLen);
+            int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &addrLen);
             if (clientSocket == -1) {
                 perror("Accept failed");
                 exit(EXIT_FAILURE);
@@ -127,7 +123,7 @@ void runServer(int serverSocket){
                 memset(buffer, 0, sizeof(buffer));
                 ssize_t bytesReceived = receiveMessage(clientSocket, buffer);
 
-                printf ("%s\n", buffer);
+                printf("%s\n", buffer);
             }
         }
     }
@@ -140,16 +136,16 @@ void runServer(int serverSocket){
     }
 }
 
-int main(int argc, char *argv[]){
-	if (argc != 2) {
-		printf("Usage: %s PortNumber\n", argv[0]);
-		exit(1);
-	}
-    
-    int serverSocket = atoi(argv[1]);
-    int clientSocket = 0;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s PortNumber\n", argv[0]);
+        exit(1);
+    }
 
-    initializeServer(serverSocket);
+    int serverSocket;
+    int port = atoi(argv[1]);
+
+    initializeServer(&serverSocket, port);
 
     runServer(serverSocket);
 
