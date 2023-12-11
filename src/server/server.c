4e82c297047ad6325bc650d8da8386cb922916e2
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-11 23:21:12 +0700

    Update server.c

--- src/server/server.c
+++ src/server/server.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFFER 1024
 #include "../../include/server.h"
 #include "../../include/shared/constants.h"
 // #include "../../include/server/log.h"
