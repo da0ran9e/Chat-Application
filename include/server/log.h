cc1a387f0e78c908a39856b93cf07259f3b02d48
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-12 10:56:09 +0700

    Update log.h

--- include/server/log.h
+++ include/server/log.h
@@ -12,9 +12,23 @@
 
 #define MAX_LOG_SIZE 1024
 
+enum ServerStat {
+    CONNECT,
+    START,
+    END,
+};
+
+enum ClientStat {
+    LOGIN,
+    LOGOUT,
+    REGISTER,
+};
+
 void write_log(char *log);
-void get_log_time(char * time);
+void get_log_time(char * timeString);
 void get_username(char *ip, int id, char *name);
-void serverLog(enum ServerStatus status, int port);
-void clientLog(enum ClientStatus status, int port, char *ipAddress);
+void serverLog(enum ServerStat status, int port);
+
+void serverLog(enum ServerStat status, char *ip);
+//void clientLog(enum ClientStat status, int port, char *ipAddress);
 #endif
