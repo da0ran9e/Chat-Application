5ee72623b172f28479c9c1a484a72e7e90171419
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-12 10:34:14 +0700

    Update server.c

--- src/server/server.c
+++ src/server/server.c
@@ -150,7 +150,7 @@ int main(int argc, char *argv[]) {
 
     runServer(serverSocket);
 
-    // serverLog(RUNNING, serverSocket);
+    serverLog(RUNNING, serverSocket);
 
     close(serverSocket);
 
