c0c885e0d401c321f7ba37fbc67313f5a888b7b7
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-12 11:14:34 +0700

    Update log.h

--- include/server/log.h
+++ include/server/log.h
@@ -27,7 +27,7 @@ enum ClientStat {
 void write_log(char *log);
 void get_log_time(char * timeString);
 void get_username(char *ip, int id, char *name);
-void serverLog(enum ServerStat status, const int port);
-void connectionLog(enum ServerStat status, const int port, const char *ip);
+void serverLog(enum ServerStat status, int port);
+void connectionLog(enum ServerStat status, int port, char *ip);
 //void clientLog(enum ClientStat status, int port, char *ipAddress);
 #endif
