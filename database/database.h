d483137a5013db5d3a273ff2d8cda2a2115b785b
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-13 19:51:09 +0700

    Create database.h

---
+++ database/database.h
@@ -0,0 +1,52 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+enum LoginStatus{
+    DENY,
+    ACCEPT
+};
+
+enum RegisterStatus{
+    USERNAME_CONFLICT,
+    SERVER_FULL,
+    DONE
+};
+
+enum PasswordInChangeStatus{
+    DONE,
+    DENY
+};
+
+enum AddFriendStatus{
+    DONE,
+    ERROR
+};
+
+enum DeleteFiendStatus{
+    DONE,
+    ERROR
+};
+
+enum CreateRoomStatus{
+    DONE,
+    ERROR
+};
+
+enum AddMemberStatus{
+    DONE,
+    MEMBER_CONFLICT,
+    ROOM_FULL
+};
+
+enum RemoveMemberStatus{
+    DONE,
+    MEMBER_NOTFOUND,
+};
+
+PGconn * database_start ();
+void database_exit(PGconn *conn);
+
+#endif
