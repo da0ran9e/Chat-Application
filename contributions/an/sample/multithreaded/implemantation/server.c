93d9716f2f69a40b991684142efa50851aaa5fa7
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-17 00:00:38 +0700

    Update server.c

--- contributions/an/sample/multithreaded/implemantation/server.c
+++ contributions/an/sample/multithreaded/implemantation/server.c
@@ -92,7 +92,9 @@ void *handleClient(void *args) {
         }
 
         // Process the received message
-        printf("Processing message from client %d: %s\n", clientSocket, buffer);
+        printf("Processing message from client %d: \n", clientSocket);
+
+        read(buffer);
 
         // Echo the message back to the client
         sendMessage(clientSocket, buffer, strlen(buffer));
@@ -144,6 +146,112 @@ void runServer(int serverSocket) {
         // Detach the thread to clean up resources automatically
         pthread_detach(thread);
     }
+}
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Parameters{
+    char Param1[1024];
+    char Param2[1024];
+    char Param3[1024];
+} Parameters;
+
+void substring(const char *string, char *buffer, int pos, int len)
+{
+    char * temp = (char *) malloc((len + 1)*sizeof(char));
+    strcpy(temp, string + pos);
+    temp[len] = '\0';  // Null-terminate the string
+    strcpy(buffer, temp);
+}
+
+uint32_t getProtocolOpcode (const char *message){
+    // Extract the integer from the first 4 bytes
+    uint32_t opCode;
+    memcpy(&opCode, message, sizeof(opCode));
+    return opCode;
+}
+
+uint32_t getProtocolFunctionCode (const char *message){
+    // Extract the integer from the next 4 bytes
+    uint32_t func;
+    memcpy(&func, message + sizeof(uint32_t), sizeof(func));
+    return func;
+}
+
+void getProtocolPayload (const char *message, char * buffer, int size){
+    // Extract the payload from the entire of the message
+    memcpy(buffer, message + 2*sizeof(uint32_t), size);
+}
+
+Parameters getProtocolParameters(const char *payload, Parameters parameters){
+    // Extract parameters from the payload 
+    uint32_t paramLen1;
+    uint32_t paramLen2;
+    uint32_t paramLen3;
+    memcpy(&paramLen1, payload, sizeof(paramLen1));
+    memcpy(&paramLen2, payload + sizeof(uint32_t) + paramLen1, paramLen2);
+    memcpy(&paramLen3, payload + 2*sizeof(uint32_t) + paramLen1 + paramLen2, paramLen3);
+
+    substring(payload, parameters.Param1, 4, paramLen1);
+    substring(payload, parameters.Param2, 8+paramLen1, paramLen2);
+    substring(payload, parameters.Param3, 12+paramLen1+paramLen2, paramLen3);
+    return parameters;
+}
+
+void undoTranslation(uint32_t intValue, char charValue, char* undoBuffer, size_t bufferSize) {
+    // Copy the integer into the buffer
+    memcpy(undoBuffer, &intValue, sizeof(intValue));
+
+    // Copy the character into the buffer after the integer
+    memcpy(undoBuffer + sizeof(intValue), &charValue, sizeof(charValue));
+}
+
+void generateMessage(uint32_t op, uint32_t func, Parameters parameters, char * buffer){
+    int len1 = strlen(parameters.Param1);
+    int len2 = strlen(parameters.Param2);
+    int len3 = strlen(parameters.Param3);
+
+    int bufferSize = 20 + len1 + len2 + len3 + 1;
+
+    memcpy(buffer, &op, sizeof(op));
+    memcpy(buffer+4, &func, sizeof(func));
+    memcpy(buffer+8, (uint32_t*)&len1, sizeof(uint32_t));
+    memcpy(buffer+12, parameters.Param1, len1*sizeof(uint32_t));
+    memcpy(buffer+12+len1, (uint32_t*)&len2, sizeof(uint32_t));
+    memcpy(buffer+16+len1, parameters.Param2, len2*sizeof(uint32_t));
+    memcpy(buffer+16+len1+len2, (uint32_t*)&len2, sizeof(uint32_t));
+    memcpy(buffer+20+len1+len2, parameters.Param3, len3*sizeof(uint32_t));
+    buffer[bufferSize-1] = '\0';
+}
+
+int read(const char * binaryString, int size) {
+    printf("Opcode: %d\n", getProtocolOpcode(binaryString));
+    printf("Func: %d\n", getProtocolFunctionCode(binaryString));
+    char payload[size];
+
+    getProtocolPayload(binaryString, payload, sizeof(payload));
+    Parameters p;
+    p = getProtocolParameters(payload, p);
+
+    printf("Param1: %s\n", p.Param1);
+    printf("Param2: %s\n", p.Param2);
+    printf("Param3: %s\n", p.Param3);
+
+    printf("len1: %d\n", strlen(p.Param1));
+
+    // Print the binary string
+
+    printf("Binary String : \n");
+    for (size_t i = 0; i < sizeof(binaryString); i++) {
+        printf("\\x%02X", (unsigned char)binaryString[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
