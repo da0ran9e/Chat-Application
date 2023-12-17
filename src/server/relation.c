a29bb3dc5fe93dd76f16deeb6a1fba857f3dac29
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-17 17:38:14 +0700

    Update relation.c

--- src/server/relation.c
+++ src/server/relation.c
@@ -1,5 +1,23 @@
-#include "../../database/database.h"
+#include "feature.h"
 
-int s_rela_friend_list (const char * username, char * friendlist, int count);
-int s_rela_friend_request (const char * username, const char * destination);
-int s_rela_friend_response (const char * username, char * destination, const char response);
+/*
+* 201: success
+* 101: fail
+*/
+int s_rela_friend_list (const char * username, char * friendlist, int count){
+    count = dbc_get_friendlist(username, friendlist);
+    if (count > 0) return 202;
+    else return 102;
+}
+
+/*
+* 211: success
+* 111: fail
+* 411: connection lost
+*/
+int s_rela_friend_request (const char * username, const char * destination){
+
+}
+int s_rela_friend_response (const char * username, char * destination, const char response){
+
+}
