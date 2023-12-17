4f3737196c44ec27769ba2c23707d639b6a0a0cf
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-17 18:14:29 +0700

    Update common.h

--- include/shared/common.h
+++ include/shared/common.h
@@ -1,5 +1,29 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+#define BUFFER 1024
+
+typedef struct Parameters{
+    char Param1[BUFFER];
+    char Param2[BUFFER];
+    char Param3[BUFFER];
+} Parameters;
+
+typedef struct Room{
+    int roomId;
+    char roomName[50];
+}Room;
+
+typedef struct Message{
+    int roomId;
+    int userId;
+    char timestamp[20];
+    char content[500];
+}Message;
 #endif
