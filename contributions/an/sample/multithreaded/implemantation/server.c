6134b244d7322c73daa9252b75599a2d5a58b4e9
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-17 10:28:22 +0700

    Update server.c

--- contributions/an/sample/multithreaded/implemantation/server.c
+++ contributions/an/sample/multithreaded/implemantation/server.c
@@ -173,6 +173,11 @@ void runServer(int serverSocket) {
 
 void substring(const char *string, char *buffer, int pos, int len)
 {
+    // printf("Binary String : \n");
+    for (size_t i = 0; i < len; i++) {
+        printf("\\x%02X", (unsigned char)string[i]+pos);
+    }
+    printf("\n");
     char * temp = (char *) malloc((len + 1)*sizeof(char));
     strcpy(temp, string + pos);
     temp[len] = '\0';  // Null-terminate the string
@@ -204,12 +209,18 @@ Parameters getProtocolParameters(const char *payload, Parameters parameters){
     uint32_t paramLen2;
     uint32_t paramLen3;
     memcpy(&paramLen1, payload, sizeof(paramLen1));
-    memcpy(&paramLen2, payload + sizeof(uint32_t) + paramLen1, paramLen2);
-    memcpy(&paramLen3, payload + 2*sizeof(uint32_t) + paramLen1 + paramLen2, paramLen3);
+    memcpy(&paramLen2, payload + sizeof(uint32_t) + paramLen1, sizeof(paramLen2));
+    memcpy(&paramLen3, payload + 2*sizeof(uint32_t) + paramLen1 + paramLen2, sizeof(paramLen3));
+    // printf("ParamLen1: %d\n", paramLen1);
+    // printf("ParamLen2: %d\n", paramLen2);
+    // printf("ParamLen3: %d\n", paramLen3);
 
     substring(payload, parameters.Param1, 4, paramLen1);
     substring(payload, parameters.Param2, 8+paramLen1, paramLen2);
     substring(payload, parameters.Param3, 12+paramLen1+paramLen2, paramLen3);
+    // printf("Param1: %s\n", parameters.Param1);
+    // printf("Param2: %s\n", parameters.Param2);
+    // printf("Param3: %s\n", parameters.Param3);
     return parameters;
 }
 
@@ -225,6 +236,9 @@ void generateMessage(uint32_t op, uint32_t func, Parameters parameters, char * b
     int len1 = strlen(parameters.Param1);
     int len2 = strlen(parameters.Param2);
     int len3 = strlen(parameters.Param3);
+    printf("ParamLen1: %d\n", len1);
+    printf("len2: %d\n", len2);
+    printf("len3: %d\n", len3);
 
     int bufferSize = 20 + len1 + len2 + len3 + 1;
 
@@ -234,7 +248,7 @@ void generateMessage(uint32_t op, uint32_t func, Parameters parameters, char * b
     memcpy(buffer+12, parameters.Param1, len1*sizeof(uint32_t));
     memcpy(buffer+12+len1, (uint32_t*)&len2, sizeof(uint32_t));
     memcpy(buffer+16+len1, parameters.Param2, len2*sizeof(uint32_t));
-    memcpy(buffer+16+len1+len2, (uint32_t*)&len2, sizeof(uint32_t));
+    memcpy(buffer+16+len1+len2, (uint32_t*)&len3, sizeof(uint32_t));
     memcpy(buffer+20+len1+len2, parameters.Param3, len3*sizeof(uint32_t));
     buffer[bufferSize-1] = '\0';
 }
