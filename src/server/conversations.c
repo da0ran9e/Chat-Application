c276eadd989ca244dace479d7f2f51ca29848d57
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-17 17:38:10 +0700

    Update conversations.c

--- src/server/conversations.c
+++ src/server/conversations.c
@@ -1,4 +1,36 @@
-#include "../../database/database.h"
+#include "feature.h"
 
-int s_conv_get_conversation (const int room_id, const char *timestamp);
-int s_conv_new_message (const char *username, const int room_id, const char *message);
+/*
+* 203: success
+* 103: fail
+*/
+int s_conv_get_conversation (const int roomId, const char *timestamp, char * messageList, int count){
+    if (timestamp == NULL){
+        count = dbc_get_new_conversation(roomId, messageList);
+    }
+    else{
+        count = dbc_get_prev_conversation(roomId, timestamp, messageList);
+    }
+    if (count > 0) return 203;
+    else return 103;
+}
+
+/*
+* 213: success
+* 113: fail
+* 413: connection lost
+*/
+int s_conv_new_message (const char *username, const int roomId, const char *message){
+    switch (dbc_new_message(username, roomId, message))
+    {
+    case ACCEPT:
+        return 213;
+        break;
+    case ERROR:
+        return 113;
+        break;
+    default:
+        return 413;
+        break;
+    }
+}
