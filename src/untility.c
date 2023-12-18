388ae3345e6103b71b7edc146dfacda49e5d1549
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-18 12:36:16 +0700

    Update untility.c

--- src/untility.c
+++ src/untility.c
@@ -13,7 +13,7 @@ void util_get_time(char * timeString){
 }
 
 // get a substring from a binary string
-void util_substring(const char *string, char *buffer, int pos, int len)
+void util_get_substring(const char *string, char *buffer, int pos, int len)
 {
     char * temp = (char *) malloc((len + 1)*sizeof(char));
     strcpy(temp, string + pos);
