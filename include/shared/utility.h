70944cafda4b98658183e9e346bcf97b193e550f
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-18 13:08:01 +0700

    Update utility.h

--- include/shared/utility.h
+++ include/shared/utility.h
@@ -1,6 +1,12 @@
 #ifndef ULTILITY_H
 #define ULTILITY_H
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
 // get current timestamp as string (format: YYYY-mm-dd HH:MM:SS)
 void util_get_time(char * timeString);
 // get a substring from a binary string
