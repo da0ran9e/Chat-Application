7d24e9c0ecfef8acf2e3f56c71a23b6a6a44f7dd
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-20 18:19:01 +0700

    Update log.h

--- include/server/log.h
+++ include/server/log.h
@@ -24,6 +24,6 @@ void write_log(const char *log);
 void get_username(const char *ip, int id);
 void serverLog(enum ServerStat status, int port);
 void connectionLog(enum ServerStat status, int port, const char *ip);
-//void clientLog(enum ClientStat status, int port, char *ipAddress);
+void clientLog(enum ClientStat status, const char * username, const char *ipAddress,const int port);
 
 #endif
