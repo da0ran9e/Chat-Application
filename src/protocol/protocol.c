22eaf089aa8ac7db606292bce03fe2ab68c40fb1
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-27 00:00:46 +0700

    Update protocol.c

--- src/protocol/protocol.c
+++ src/protocol/protocol.c
@@ -35,24 +35,26 @@ void getProtocolParameters(const char *payload, Parameters *parameters) {
     util_get_substring(payload, parameters->Param3, 12 + paramLen1 + paramLen2, paramLen3);
 }
 
-int generateMessage(uint32_t op, uint32_t func, Parameters parameters, char *buffer) {
-    uint32_t len1 = strlen(parameters.Param1);
-    uint32_t len2 = strlen(parameters.Param2);
-    uint32_t len3 = strlen(parameters.Param3);
-
+int generateMessage(uint32_t op, uint32_t func, Parameters parameters, char * buffer){
+    int len1 = strlen(parameters.Param1);
+    int len2 = strlen(parameters.Param2);
+    int len3 = strlen(parameters.Param3);
     printf("len1: %d\n", len1);
     printf("len2: %d\n", len2);
     printf("len3: %d\n", len3);
 
-    memcpy(buffer, &op, sizeof(op));
-    memcpy(buffer + 4, &func, sizeof(func));
-    memcpy(buffer + 8, &len1, sizeof(len1));
-    memcpy(buffer + 12, parameters.Param1, len1);
-    memcpy(buffer + 12 + len1, &len2, sizeof(len2));
-    memcpy(buffer + 16 + len1, parameters.Param2, len2);
-    memcpy(buffer + 16 + len1 + len2, &len3, sizeof(len3));
-    memcpy(buffer + 20 + len1 + len2, parameters.Param3, len3);
+    int bufferSize = 20 + len1 + len2 + len3;
 
-    return 20 + len1 + len2 + len3; // Total size of the message
+    memcpy(buffer, &op, sizeof(op));
+    memcpy(buffer+4, &func, sizeof(func));
+    memcpy(buffer+8, (uint32_t*)&len1, sizeof(uint32_t));
+    memcpy(buffer+12, parameters.Param1, len1*sizeof(uint32_t));
+    memcpy(buffer+12+len1, (uint32_t*)&len2, sizeof(uint32_t));
+    memcpy(buffer+16+len1, parameters.Param2, len2*sizeof(uint32_t));
+    memcpy(buffer+16+len1+len2, (uint32_t*)&len3, sizeof(uint32_t));
+    memcpy(buffer+20+len1+len2, parameters.Param3, len3*sizeof(uint32_t));
+    buffer[bufferSize-1] = '\0';
+
+    return bufferSize;
 }
 
