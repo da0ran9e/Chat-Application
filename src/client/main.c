18d1f6fa720bfe44b62f7db74ae7aad5e1dc4381
quinntd2506 <trinhdiemquynh2506@gmail.com>
2023-12-28 14:05:26 +0700

    fix x16 client

--- src/client/main.c
+++ src/client/main.c
@@ -1,7 +1,14 @@
 #include "../../include/client/feature.h"
 
-int main(){
-    run_client();
-    
+int main(int argc, char *argv[]){
+    if (argc != 3) {
+        printf("Usage: %s ServerIPAddress PortNumber\n", argv[0]);
+        exit(1);
+    }
+    char *clientIP = argv[1];
+    int clientPort = atoi(argv[2]);
+
+    run_client(clientIP,clientPort);
+
     //out_login("username", "password");
 }
