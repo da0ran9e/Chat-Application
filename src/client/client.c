a6c133d1f37a4bff91f851a6cb256dd2e396b102
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2023-12-29 10:20:10 +0700

    Update client.c

--- src/client/client.c
+++ src/client/client.c
@@ -9,7 +9,7 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-#include "../shared/common.h"
+#include "../../include/shared/common.h"
 #define MAX_CLIENTS 100
 
 enum RequestEvent {
@@ -559,4 +559,18 @@ void out_get_prev_conversation(const int roomId, const char * timestamp){
     strcpy(params.Param3, "\0"); 
     int len = generateMessage(3, 0, params, buffer);
     send_message(g_socket, buffer, len);
+}
+
+
+int main(int argc, char *argv[]){
+    if (argc != 3) {
+        printf("Usage: %s ServerIPAddress PortNumber\n", argv[0]);
+        exit(1);
+    }
+    char *clientIP = argv[1];
+    int clientPort = atoi(argv[2]);
+
+    run_client(clientIP,clientPort);
+
+    //out_login("username", "password");
 }
