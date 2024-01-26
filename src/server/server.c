c01d2d0c55e2674bee08b2a65f5034e3082acd84
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-01-26 19:35:09 +0700

    Update server.c

--- src/server/server.c
+++ src/server/server.c
@@ -13,11 +13,13 @@ void getNetworkIPAddress() {
         return;
     }
 
-        if (ifa->ifa_next->ifa_addr->sa_family == AF_INET) {
+    ifa = ifa->ifa_next;
+
+        if (ifa->ifa_addr->sa_family == AF_INET) {
             char ipstr[INET_ADDRSTRLEN];
-            struct sockaddr_in* addr = (struct sockaddr_in*)ifa->ifa_next->ifa_addr;
+            struct sockaddr_in* addr = (struct sockaddr_in*)ifa->ifa_addr;
             inet_ntop(AF_INET, &(addr->sin_addr), ipstr, INET_ADDRSTRLEN);
-            printf("Network Interface: %s\t Network IP Address: %s\n", ifa->ifa_next->ifa_name, ipstr);
+            printf("Network Interface: %s\t Network IP Address: %s\n", ifa->ifa_name, ipstr);
         }
 
     freeifaddrs(ifaddr);
