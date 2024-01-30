e33eae80398cf4214a7f50b2e0a13ec2ed9e795c
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-01-30 17:32:44 +0700

    build.shs

--- src/graphicUI/build.sh
+++ src/graphicUI/build.sh
@@ -1,7 +1,26 @@
+# Check if gcc is installed
+if ! command -v gcc &> /dev/null; then
+    echo "Installing gcc..."
+    sudo apt-get update
+    sudo apt-get install gcc
+else
+    echo "gcc is already installed."
+fi
+
+# Check if g++ is installed
+if ! command -v g++ &> /dev/null; then
+    echo "Installing g++..."
+    sudo apt-get update
+    sudo apt-get install g++
+else
+    echo "g++ is already installed."
+fi
+
 # install gtk webview
 sudo apt install libgtk-3-dev libwebkit2gtk-4.0-dev
 
 # create folder for object files
+mkdir ../../build
 mkdir ../../build/gui
 g++ -c ../../include/webview/webview.cc -std=c++11 -DWEBVIEW_STATIC $(pkg-config --cflags gtk+-3.0 webkit2gtk-4.0) -o ../../build/gui/webview.o
 
