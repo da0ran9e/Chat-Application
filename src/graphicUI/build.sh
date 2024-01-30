1601e58ddf2d56036faa8717036bfcd5a5769c0c
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-01-30 17:26:00 +0700

    setting up

--- src/graphicUI/build.sh
+++ src/graphicUI/build.sh
@@ -1,11 +1,13 @@
+# install gtk webview
+sudo apt install libgtk-3-dev libwebkit2gtk-4.0-dev
+
+# create folder for object files
 mkdir ../../build/gui
 g++ -c ../../include/webview/webview.cc -std=c++11 -DWEBVIEW_STATIC $(pkg-config --cflags gtk+-3.0 webkit2gtk-4.0) -o ../../build/gui/webview.o
 
-# gcc -c gui.c -std=c99 -Ilibs/webview -o ../../build/gui/gui.o
-# g++ ../../build/gui/gui.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o gui && gui
-
+# compile GUI
 gcc -c ui_auth.c -std=c99 -Ilibs/webview -o ../../build/gui/auth.o
-g++ ../../build/gui/auth.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o auth 
+g++ ../../build/gui/auth.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o auth && ./auth
 
 gcc -c ui_app.c -std=c99 -Ilibs/webview -o ../../build/gui/app.o
-g++ ../../build/gui/app.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o app 
+g++ ../../build/gui/app.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o app && ./app
--- src/server/build.sh
+++ src/server/build.sh
@@ -26,5 +26,9 @@ if ! sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
     sudo -u postgres psql -d $DB_NAME -U $DB_USER -f $SQL_FILE
 fi
 
+# install C lib for posgresql
+sudo apt-get install libpq-dev
+
 # server compile
 gcc -o server main.c features.c log.c authentication.c connection.c conversations.c relation.c rooms.c ../database/db_connection.c ../database/database.c ../database/sql_queries.c ../utility.c ../protocol/protocol.c -L/usr/include/postgresql/libpq -lpq
+./server 5500
