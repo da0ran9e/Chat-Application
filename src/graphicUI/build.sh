67cad6e625178dcf4745ca55efed130961f152a7
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-01-31 23:52:58 +0700

    build path change

--- src/graphicUI/build.sh
+++ src/graphicUI/build.sh
@@ -21,12 +21,17 @@ sudo apt install libgtk-3-dev libwebkit2gtk-4.0-dev
 
 # create folder for object files
 mkdir ../../build
+mkdir ../../bin
 mkdir ../../build/gui
 g++ -c ../../include/webview/webview.cc -std=c++11 -DWEBVIEW_STATIC $(pkg-config --cflags gtk+-3.0 webkit2gtk-4.0) -o ../../build/gui/webview.o
 
 # compile GUI
 gcc -c ui_auth.c -std=c99 -Ilibs/webview -o ../../build/gui/auth.o
-g++ ../../build/gui/auth.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o auth && ./auth
+g++ ../../build/gui/auth.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o ../../bin/auth
+cd ../../bin
+./auth
 
 gcc -c ui_app.c -std=c99 -Ilibs/webview -o ../../build/gui/app.o
-g++ ../../build/gui/app.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o app && ./app
+g++ ../../build/gui/app.o ../../build/gui/webview.o $(pkg-config --libs gtk+-3.0 webkit2gtk-4.0) -o ../../bin/app
+cd ../../bin
+./app
