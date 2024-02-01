bb8423fad0b8f10cfe1cee03fb3390b465fd23a7
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-02-01 09:40:26 +0700

    database fix

--- ChatPals/README.md
+++ ChatPals/README.md
@@ -10,4 +10,6 @@ chmod 755 start.sh
 ./start.sh
 
 ```
+> [!NOTE]
+> When compiling server, the terminal will ask to add a new role, add any name of role and follow the instruction (this step will not affect the application) or just add the existing role (such as 'postgres') to skip this step.
 The client executable file will be create in the `bin` folder.
--- ChatPals/src/server/build.sh
+++ ChatPals/src/server/build.sh
@@ -19,7 +19,7 @@ fi
 # database setting up
 DB_NAME="chat"
 DB_USER="postgres"
-SQL_FILE="../database/setup/setup.sql"
+SQL_FILE="setup.sql"
 
 # check postgre sql existance
 if ! command -v psql &> /dev/null; then
@@ -29,24 +29,23 @@ if ! command -v psql &> /dev/null; then
     sudo apt-get install postgresql-client
     #start posgresql service
     sudo systemctl start postgresql.service
-fi
-
-# check database
-if ! sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw $DB_NAME; then
-
     # Create a database and user
     sudo -u postgres createdb $DB_NAME
     sudo -u postgres createuser --interactive --username=$DB_USER
     # change user password  
-    ALTER USER postgres WITH PASSWORD 'anquynh123';
+    sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'anquynh123';"
 
     # Connect to the database and execute SQL script
+    cd ../database/setup
     sudo -u postgres psql -d $DB_NAME -U $DB_USER -f $SQL_FILE
+    cd ../../server
 fi
 
 # install C lib for posgresql
 sudo apt-get install libpq-dev
 
 # server compile
-gcc -o server main.c features.c log.c authentication.c connection.c conversations.c relation.c rooms.c ../database/db_connection.c ../database/database.c ../database/sql_queries.c ../utility.c ../protocol/protocol.c -L/usr/include/postgresql/libpq -lpq
+mkdir ../../bin
+gcc -o ../../bin/server main.c features.c log.c authentication.c connection.c conversations.c relation.c rooms.c ../database/db_connection.c ../database/database.c ../database/sql_queries.c ../utility.c ../protocol/protocol.c -L/usr/include/postgresql/libpq -lpq
+cd ../../bin
 ./server 5500
